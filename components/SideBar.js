32ea3ced5029ba4afeaa44b41d251398b6bfac4e
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2022-08-21 07:23:00 +0800

    sidebar restored

--- components/SideBar.js
+++ components/SideBar.js
@@ -4,9 +4,9 @@ import React from "react";
 const SideBar = () => {
   return (
     <div className="w-2/12 min-h-screen bg-gray-100">
-      <div className="h-24 flex justify-center items-center border-b border-b-1 border-b-indigo-400">
+      {/* <div className="h-24 flex justify-center items-center border-b border-b-1 border-b-indigo-400">
         Ousmane Yahya Diallo
-      </div>
+      </div> */}
       <div className="px-8 py-4 border-b border-b-1 border-b-indigo-400">
         <Link href="/admin/dash/classes">Classes</Link>
       </div>
