813a00bdae220791af1c637ae6199d99eac08224
Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>
2024-06-28 08:41:34 +0800

    components: fix price
    Signed-off-by: Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>

---
+++ components/checkout.js
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { useStudent } from "../context/StudentContext";
+
+
+const CheckoutDisplay = ({ studentData }) => {
+  const { selfRegister } = useStudent();
+  const [plan, setPlan] = useState({ plan: '' });
+  const router = useRouter();
+
+
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6">
+      <h4 className="text-2xl font-semibold mb-6">Checkout Details</h4>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="bg-gray-100 p-4 rounded-md">
+          <h5 className="text-xl font-bold mb-2">Student Information</h5>
+          <p><span className="font-semibold">Full Name:</span> {studentData.fullName}</p>
+          <p><span className="font-semibold">Phone:</span> {studentData.phone}</p>
+          <p><span className="font-semibold">Email:</span> {studentData.email}</p>
+          <p><span className="font-semibold">Whatsapp:</span> {studentData.whatsapp}</p>
+          <p><span className="font-semibold">Gender:</span> {studentData.gender}</p>
+          <p><span className="font-semibold">Language:</span> {studentData.language}</p>
+          <p><span className="font-semibold">Program:</span> {studentData.program}</p>
+        </div>
+        <div className="bg-gray-100 p-4 rounded-md">
+          <h5 className="text-xl font-bold mb-2">Plan Information</h5>
+          <p><span className="font-semibold">Title:</span> {studentData.title}</p>
+          <p><span className="font-semibold">Description:</span> {studentData.items}</p>
+          <p><span className="font-semibold">Pricing:</span> ${studentData.price}</p>
+          <div className="mt-4 relative px-12 py-4 bg-indigo-600 rounded-md text-center text-white">
+            <button onClick={() => router.replace('pricing')}>
+              View All Plans
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-end mt-6">
+        <button
+          className="px-6 py-2 rounded-md bg-indigo-500 text-white text-sm"
+          type="button"
+          onClick={() => {
+            const studData = { ...studentData, ...plan, active: false };
+            // Assuming selfRegister is defined elsewhere
+            selfRegister(studData);
+            router.replace('dashboard');
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutDisplay;
